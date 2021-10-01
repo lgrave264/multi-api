654881f0b9b4284d55b76d7abce5c42f1fe66914
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2021-10-01 13:25:33 -0700

    template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Base from './Components/Base';
+
+function App(){
+    
+    return(
+        <main>
+            <section className='container'>
+                <Base />
+            </section>
+        </main>
+    )
+}
+
+export default App

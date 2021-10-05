01f180f30e1b3ca8b05242a9de176891acdd8a59
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2021-10-05 15:20:59 -0700

    setup api categories

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 
-const content = () => {
+const Content = () => {
+	const [shows,setShows] =useState();
+	const axios = require('axios').default;
+  const [category, setCategory] = useState('action')
+  console.log(category)
+	
+ 
+
+    useEffect(()=>{
+    /* axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=${category}`) */
+    axios.get('https://kitsu.io/api/edge/anime/1/categories')
+    .then(function (response) {
+        // handle success
+      setShows(response.data.data)
+        console.log(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  },[category])
+  // shows.array.forEach(element => {
+	  
+  // });
+	
 	return (
 		<div>
-			Base
+      <ul id='catlist'>
+      <li><button className='genre' onClick={()=>{setCategory('action');}}>Action</button></li>
+      <li><button className='genre' onClick={()=>{setCategory('adventure')}}>Adventure</button></li>
+      <li><button className='genre' onClick={()=>{setCategory('drama')}}>drama</button></li>
+      <li><button className='genre' onClick={()=>{setCategory('fantasy')}}>fantasy</button></li>
+      <li><button className='genre' onClick={()=>{setCategory('sci-fi')}}>sci-fi</button></li>
+    </ul>
+			
 		</div>
 	)
 }
 
-export default content
+export default Content
